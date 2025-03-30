64fc3ba9a32542b8566503a0209386b28c3d9165
Rasmus Lystrøm <rasmusl@microsoft.com>
2025-03-30 10:52:24 +0200

    Configure Web API Auth

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 ```bash
 dotnet new blazorwasm -au SingleOrg --client-id "ee66e8dc-c387-403c-876d-2195137d094b" --output "src/Frontend" --tenant-id "b461d90e-0c15-44ec-adc2-51d14f9f5731"
 ```
+
+## Tutorial 2
+
+[Quickstart: Call a web API that is protected by the Microsoft identity platform](https://learn.microsoft.com/en-us/entra/identity-platform/quickstart-web-api-aspnet-sign-in?tabs=aspnet-core-workforce)
+
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.TokenValidationParameters.NameClaimType = "name";
+    }, options => { builder.Configuration.Bind("AzureAd", options); });
+
+// Configure authorization
+builder.Services.AddAuthorization(config =>
+{
+config.AddPolicy("AuthZPolicy", policy =>
+    policy.RequireRole("Forecast.Read"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("weatherForecast")
+.RequireAuthorization("AuthZPolicy"); // Protect this endpoint with the AuthZPolicy
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
